ef88b4e7ea24036c8531cb207b8837072b89bdc4
Kamil Kapturski <kamil3392@o2.pl>
2015-03-24 14:08:02 +0000

    don't work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :models
   #get 'welcome/index'
   resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
